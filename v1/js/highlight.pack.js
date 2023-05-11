18eee06c2cd1bea4666281cd9064c3ca654735ac
joente <joente@users.noreply.github.com>
2023-05-11 12:15:03 +0000

    deploy: 4ff5f24cd05f8e38fb22e8170f7592eefb793bf1

--- v1/js/highlight.pack.js
+++ v1/js/highlight.pack.js
@@ -1896,7 +1896,7 @@ hljs.registerLanguage('thingsdb', function () {
                     className: 'function',
                     begin: new RegExp(
                         '\\.\\s*(assign|args|set_args|len|load|call|choice|closure|set_closure|copy|dup|join|doc|emit|code|msg|each|every|extend|extend_unique|filter|find|' +
-                        'find_index|index_of|map|map_id|map_wrap|map_type|vmap|name|owner|set_owner|pop|push|fill|remove|replace|reverse|restrict|restriction|split|sort|splice|unique|is_unique|' +
+                        'find_index|index_of|flat|map|map_id|map_wrap|map_type|vmap|name|owner|set_owner|pop|push|fill|remove|replace|reverse|restrict|restriction|split|sort|splice|unique|is_unique|' +
                         'at|again_in|again_at|cancel|add|one|err|clear|has|contains|ends_with|lower|starts_with|test|trim|trim_left|trim_right|upper|' +
                         'del|ren|to_type|to_thing|get|id|keys|reduce|set|shift|some|value|values|wrap|unshift|unwrap|' +
                         'search|extract|first|last|equals|then|else|format|move|to|week|weekday|yday|zone)\\s*(?=\\()'
